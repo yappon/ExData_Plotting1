81f337736e21dc8f0da958cc1a6fae6a7553c876
Yasuhiro Ito <Yasuhiro.Ito.771105@gmail.com>
2015-09-12 23:51:23 +0900

    plot1 submission

--- plot1.R
+++ plot1.R
@@ -28,5 +28,5 @@ remove(subsetData)
 hist(cleanedData$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
 
 ## Step.4 Save to png file
-dev.copy(png, "rplot1.png", width = 480, height = 480)
+dev.copy(png, "plot1.png", width = 480, height = 480)
 dev.off()
